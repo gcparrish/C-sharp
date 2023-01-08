ce9ffbd748be005e0c6e0a04171ceb4a8b3312fd
gcparrish <gcparrish908@gmail.com>
2023-01-08 15:14:25 -0500

    Finished part one

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number?");
+        string magicInput = Console.ReadLine();
+        int magicNumber = int.Parse (magicInput);
+        Console.Write("What is your guess?");
+        string userGuess = Console.ReadLine();
+        int guess = int.Parse (userGuess);
+        if (guess > magicNumber)
+        {
+            Console.Write("Lower.");
+        }
+        else if (guess < magicNumber)
+        {
+            Console.Write("Higher.");
+        }
+        else
+        {
+            Console.Write("You got it!");
+        }
     }
 }
