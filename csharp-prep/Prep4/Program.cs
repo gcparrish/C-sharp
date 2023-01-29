8795789161b042cbbbabbcd36f89d4a96bc5d4ac
gcparrish <gcparrish908@gmail.com>
2023-01-29 13:13:52 -0500

    Created Skeleton for Task to add the math to later

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+        int number;
+        do
+        {
+            Console.Write ("Enter a number: ");
+            string listEntry = Console.ReadLine ();
+            number = int.Parse (listEntry);
+            numbers.Add(number);
+        } while (number != 0);
+        int total = 9;
+        int average = total / numbers.Count;
+        int maximum = 7;
+        Console.Write ("Your total is ");
+        Console.WriteLine (total);
+        Console.Write ("Your average is ");
+        Console.WriteLine (average);
+        Console.Write ("Your maximum is ");
+        Console.WriteLine (maximum);
     }
 }
