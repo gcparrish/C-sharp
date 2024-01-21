c4849dd34ea7d5e620fe0b724b920244c9947ccd
gcparrish <gcparrish908@gmail.com>
2024-01-21 13:54:59 -0500

    Added first three functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,27 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.Write ("Welcome to the program.");
+    }
+    static string PromptUserName ()
+    {
+        Console.WriteLine ("What is your name?");
+        string user_name = Console.ReadLine();
+        return user_name;
+    }
+    static int PromptUserNumber ()
+    {
+        Console.WriteLine ("What is your favorite number?");
+        string user_number = Console.ReadLine ();
+        int favorite = int.Parse (user_number);
+        return favorite;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+       DisplayWelcome();
+       PromptUserName();
+       PromptUserNumber();
     }
 }
