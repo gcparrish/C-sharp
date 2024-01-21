dfa5d412217632e2b60bc0ebf863de597261d03c
gcparrish <gcparrish908@gmail.com>
2024-01-21 14:32:08 -0500

    finished last two functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void DisplayWelcome()
     {
-        Console.Write ("Welcome to the program.");
+        Console.WriteLine ("Welcome to the program.");
     }
     static string PromptUserName ()
     {
@@ -15,14 +15,26 @@ class Program
     static int PromptUserNumber ()
     {
         Console.WriteLine ("What is your favorite number?");
-        string user_number = Console.ReadLine ();
-        int favorite = int.Parse (user_number);
+        string user_number = Console.ReadLine();
+        
         return favorite;
+    }
+    static int SquareUserNumber (int favorite)
+    {
+        int square = favorite*favorite;
+        return square;
+    }
+    static void DisplayResult (string user_name,int square)
+    {
+        Console.WriteLine ($"Your name is: {user_name}");
+        Console.WriteLine ($"Your favorite number squared is: {square}");
     }
     static void Main(string[] args)
     {
        DisplayWelcome();
        PromptUserName();
        PromptUserNumber();
+       SquareUserNumber(PromptUserNumber());
+       DisplayResult(PromptUserName(), SquareUserNumber(PromptUserNumber()));
     }
 }
